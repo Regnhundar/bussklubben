cd72d68c32c36b9035c2bcae74fe236241095988
Magnus Nilsson <regnhundar@gmail.com>
2025-01-25 21:07:33 +0100

    Logic working, will now start the ability buttons

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@
 // Hur mycket tid man har tills game over när spelet startar.
 export const TOTAL_TIME = 30;
 // Hur mycket tid man får när man klarat en bana
-export const BONUS_TIME = 10;
+export const BONUS_TIME = 15;
 // Hur mycket tid man har på sig tills bussen åker när bana startar.
 export const PREPARATION_TIME = 15;
 // Hur lång tid bussen tar per ruta.
