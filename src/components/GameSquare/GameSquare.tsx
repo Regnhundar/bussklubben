9b892b42c16111e0ad9ebe9ae34445313800e852
Magnus Nilsson <regnhundar@gmail.com>
2025-01-31 00:01:06 +0100

    main fixed

--- src/components/GameSquare/GameSquare.tsx
+++ src/components/GameSquare/GameSquare.tsx
@@ -71,15 +71,6 @@ const GameSquare: React.FC<Props> = ({ squareData, index, finishIndicator, start
         }
     };
 
-    const childVariant = {
-        hidden: { opacity: 0, scale: 0.8 },
-        show: (customDelay: number) => ({
-            opacity: 1,
-            scale: 1,
-            transition: { duration: 0.5, delay: customDelay },
-        }),
-    };
-
     return squareData.isRevealed ? (
         <motion.img
             variants={squareImgVariant}
@@ -120,7 +111,6 @@ const GameSquare: React.FC<Props> = ({ squareData, index, finishIndicator, start
             initial='hidden'
             animate='show'
             exit='exit'
-            variants={childVariant}
             custom={squareData.delay}
             data-index={index}
             className='game-square'
