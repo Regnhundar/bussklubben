d97e9103f79d37ea57c9f689a611b9f8b6e64af2
Magnus Nilsson <regnhundar@gmail.com>
2025-02-14 12:01:18 +0100

    Can now correctly disconnect from board if starting square isn't connected

--- src/components/PathControl/PathControl.tsx
+++ src/components/PathControl/PathControl.tsx
@@ -1,24 +1,48 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useGameBoardStore from '../../stores/gameBoardStore';
 import { checkForOutOfBounds, determineDirection, squareToCheck } from '../../utils/utilityFunctions';
+import useGameStore from '../../stores/gameStore';
+import { GameBoardIndices } from '../../types/type';
 
 const PathControl: React.FC = () => {
-    const { gameBoardArray, arrivalIndex, startingIndex } = useGameBoardStore();
+    const { gameBoardArray, setGameBoardArray, arrivalIndex, startingIndex } = useGameBoardStore();
+    const { isGameOver, isGameRunning } = useGameStore();
+    const [connectedIndices, setConnectedIndices] = useState<GameBoardIndices[]>([]);
+    const isGameStarted = isGameRunning && !isGameOver && startingIndex !== null && arrivalIndex !== null;
+
+    const isFirstSquareConnected = isGameStarted
+        ? gameBoardArray[startingIndex].tile.connections[arrivalIndex] === true
+        : false;
 
     useEffect(() => {
-        if (startingIndex !== null && arrivalIndex !== null) {
-            const isFirstSquareConnected = gameBoardArray[startingIndex].tile.connections[arrivalIndex] === true;
-
-            if (isFirstSquareConnected) {
-                // const direction = determineDirection(nextSquareToCheckIndex, arrivalIndex);
-                // const willArriveFrom = direction === 0 ? 2 : direction === 2 ? 0 : direction === 1 ? 3 : 1;
-                // const isOutOfBounds = checkForOutOfBounds(nextSquareToCheckIndex, direction);
-                // const nextSquare = squareToCheck(nextSquareToCheckIndex, direction);
-            }
-
-            console.log('Array is changed.');
+        if (isGameStarted && isFirstSquareConnected) {
+            const direction = determineDirection(startingIndex, arrivalIndex);
+            const willArriveFrom = direction === 0 ? 2 : direction === 2 ? 0 : direction === 1 ? 3 : 1;
+            const nextSquare = squareToCheck(startingIndex, direction);
+            // const isOutOfBounds = checkForOutOfBounds(nextSquareToCheckIndex, direction);
+            console.log('connecting');
         }
     }, [gameBoardArray]);
+
+    const handleSquareDirection = (squareIndex: GameBoardIndices) => {
+        if (arrivalIndex) {
+            const direction = determineDirection(squareIndex, arrivalIndex);
+            const willArriveFrom = direction === 0 ? 2 : direction === 2 ? 0 : direction === 1 ? 3 : 1;
+            const nextSquare = squareToCheck(squareIndex, direction);
+            return nextSquare;
+        }
+    };
+
+    useEffect(() => {
+        if (isGameStarted && !isFirstSquareConnected) {
+            const disconnectedGameBoard = gameBoardArray.map((gameSquare) => ({
+                ...gameSquare,
+                isLinkedToStart: false,
+            }));
+            setGameBoardArray(disconnectedGameBoard);
+            console.log('disconnecting');
+        }
+    }, [isFirstSquareConnected]);
 
     return null;
 };
